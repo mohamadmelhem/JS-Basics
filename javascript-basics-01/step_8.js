e1326b66914e16b14f10a0a5aa8e2a40d41082ba
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-07 15:45:35 +0200

    step_8

---
+++ javascript-basics-01/step_8.js
@@ -0,0 +1,9 @@
+function functionA(){
+    let num1 = document.getElementById("age").value;
+    if (num1 < 18) {
+        alert("you are under 18");
+    }
+    else {
+        alert("you are over 18");
+    }
+}
