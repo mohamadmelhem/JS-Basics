3ab15e336975e33725d960489a76a6b519e862f7
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-07 14:52:48 +0200

    step 5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,5 @@
+function multiplyBy() {
+    num1 = document.getElementById("first_number").value;
+    num2 = document.getElementById("second_number").value;
+    alert("The result= " + num1 * num2);
+}
