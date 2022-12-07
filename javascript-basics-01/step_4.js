cebf9648fa79ab31347c14ddbb11375f0e6e918d
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-07 11:57:54 +0200

    step 4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,8 @@
+function myFunction() {
+let name = document.getElementById("name").value;
+let surname = document.getElementById("surname").value;
+let city = document.getElementById("city").value;
+
+
+    alert("hello " + " " +  name + " " + surname + " " + city);
+}
