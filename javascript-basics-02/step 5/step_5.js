440ad8f5bb8f7c4e2863f463199d130d6b9b79bb
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-08 13:06:14 +0200

    step 5 basics 2

---
+++ javascript-basics-02/step 5/step_5.js
@@ -0,0 +1,6 @@
+var image= document.querySelectorAll("img");
+for (let i=0;i<=image.length;i++){
+    image[i].addEventListener("mouseover", function(){
+        document.getElementById("image"+ (i+1)).src="images/image" + (i+1)+ "_2.jpg"
+    })
+}
